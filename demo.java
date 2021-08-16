469d5ef6e7222d071f46da6d230f26516d710bd3
Sayli Shrungare <sayli.shrungare@apisero.com>
2021-08-16 14:12:06 +0530

    Commit #24 number 24

--- demo.java
+++ demo.java
@@ -18,11 +18,12 @@ public class Demo {
 		System.out.println("I am Line No 14 for Jenkins");
 		System.out.println("I am Line No 15 for Jenkins using push");
 		System.out.println("I am Line No 16 for Jenkins build triggers");
-		System.out.println("I am Line No 17 for Jenkins build Poll SCM");
+		System.out.println("I am Line No 19 for Jenkins build Poll SCM");
+		System.out.println("I am Line No 18 for Jenkins build Poll SCM");
     	}
 }
 /*hello
 world
 hello again
 hello from bitbucket*/
-cd C:\Users\saylishrungare\Desktop\Training
+
