624853954b70ef3eaaa9f2b0d2943da1bdd2542f
Sayli Shrungare <sayli.shrungare@apisero.com>
2021-08-16 12:22:29 +0530

    1st commit

--- demo.java
+++ demo.java
@@ -1,25 +1,23 @@
 public class Demo {
-
-    public static void main(String[] args) {
-
-        System.out.println("I am Line No 1");
-	System.out.println("I am Line No 2 from copy1");
-System.out.println("I am Line No 1 from api");
-System.out.println("I am Line No 2 from api");
-System.out.println("I am Line No 3 from api");
-System.out.println("I am Line No 4 from api");
-System.out.println("I am Line No 5 from api");
-System.out.println("I am Line No 6 from api");
-System.out.println("I am Line No 7 for Jenkins");
-System.out.println("I am Line No 8 for Jenkins");
-System.out.println("I am Line No 9 for Jenkins");
-System.out.println("I am Line No 10 for Jenkins");
-System.out.println("I am Line No 11 for Jenkins");
-System.out.println("I am Line No 12 for Jenkins");
-System.out.println("I am Line No 13 for Jenkins");
-System.out.println("I am Line No 14 for Jenkins");
-System.out.println("I am Line No 15 for Jenkins using push");
-    }
+	public static void main(String[] args){
+		System.out.println("I am Line No 1");
+		System.out.println("I am Line No 2 from copy1");
+		System.out.println("I am Line No 1 from api");
+		System.out.println("I am Line No 2 from api");
+		System.out.println("I am Line No 3 from api");
+		System.out.println("I am Line No 4 from api");
+		System.out.println("I am Line No 5 from api");
+		System.out.println("I am Line No 6 from api");
+		System.out.println("I am Line No 7 for Jenkins");
+		System.out.println("I am Line No 8 for Jenkins");
+		System.out.println("I am Line No 9 for Jenkins");
+		System.out.println("I am Line No 10 for Jenkins");
+		System.out.println("I am Line No 11 for Jenkins");
+		System.out.println("I am Line No 12 for Jenkins");
+		System.out.println("I am Line No 13 for Jenkins");
+		System.out.println("I am Line No 14 for Jenkins");
+		System.out.println("I am Line No 15 for Jenkins using push");
+    	}
 }
 /*hello
 world
